b2d868b26b03fd89419da63fcf72207a40cb662f
sian022 <delacruzsian22@gmail.com>
2024-08-13 16:58:06 +0800

    feat: trying to inject widget

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,26 @@
-import { render } from "preact";
-import "./styles/main.scss";
+// src/index.jsx
+import { h, render } from "preact";
 import Widget from "./components/Widget";
+import "./styles/main.scss";
 
-export function App() {
-  return <Widget />;
-}
+// Create a container for your widget
+const container = document.createElement("div");
+container.id = "widget-root";
+container.style.position = "fixed";
+container.style.bottom = "20px";
+container.style.right = "20px";
+container.style.zIndex = "1000";
+document.body.appendChild(container);
 
-render(<App />, document.getElementById("app"));
+// Render your Preact app into the container
+render(<Widget />, document.getElementById("widget-root"));
+
+// import { render } from "preact";
+// import "./styles/main.scss";
+// import Widget from "./components/Widget";
+
+// export function App() {
+//   return <Widget />;
+// }
+
+// render(<App />, document.getElementById("app"));
